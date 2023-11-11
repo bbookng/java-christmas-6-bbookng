71a3d632b805069f70f5b14dba47cfeef32411cc
bbookng <bbookng@gmail.com>
2023-11-11 23:53:22 +0900

    feat(common) : 상수 클래스 생성 - Constant - ErrorMessages - Messages

---
+++ src/main/java/christmas/common/Constant.java
@@ -0,0 +1,5 @@
+package christmas.common;
+
+public class Constant {
+    public static int[] STARRED_DAY = new int[] {3, 10, 17, 24, 25, 31};
+}
---
+++ src/main/java/christmas/common/ErrorMessages.java
@@ -0,0 +1,6 @@
+package christmas.common;
+
+public class ErrorMessages {
+    public static String validateInRange = "[ERROR] 날짜는 1에서 31 사이어야 합니다.";
+    public static String validateIsNumber = "[ERROR] 숫자를 입력하세요.";
+}
