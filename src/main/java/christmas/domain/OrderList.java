2fa8b7aa6282219d8b56140003c37f91a558d317
bokyung <bbookng@gmail.com>
2023-11-13 12:27:48 +0900

    feat(OrderList) : OrderList ToString Method Override

--- src/main/java/christmas/domain/OrderList.java
+++ src/main/java/christmas/domain/OrderList.java
@@ -4,4 +4,20 @@ import java.util.List;
 
 public class OrderList {
     List<Order> orderList;
+
+    public OrderList(List<Order> orderList) {
+        this.orderList = orderList;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder message = new StringBuilder();
+        message.append("<주문 메뉴>\n");
+        orderList.forEach(order -> message.append(orderToString(order)));
+        return message.toString();
+    }
+
+    private String orderToString(Order order) {
+        return String.format("%s %d개\n", order.getMenu(), order.getCount());
+    }
 }
