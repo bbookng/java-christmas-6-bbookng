8290d5ea6fcc966417fcf70d8de739eeecd0294d
bokyung <bbookng@gmail.com>
2023-11-13 12:25:42 +0900

    fix(ErrorMessages) :ErrorMessages 조건에 맞는 형식으로 변경

--- src/main/java/christmas/common/ErrorMessages.java
+++ src/main/java/christmas/common/ErrorMessages.java
@@ -1,6 +1,6 @@
 package christmas.common;
 
 public class ErrorMessages {
-    public static String validateInRange = "[ERROR] 날짜는 1에서 31 사이어야 합니다.";
-    public static String validateIsNumber = "[ERROR] 숫자를 입력하세요.";
+    public static String validateVisitDay = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+    public static String validateOrderMenu = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
 }
