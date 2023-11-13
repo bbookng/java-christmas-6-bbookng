a167391a06aa664ced9d55a35d83c676f440697e
bokyung <bbookng@gmail.com>
2023-11-14 08:58:25 +0900

    feat(Badge) : Badge 가격대에 맞는 생성 메서드

--- src/main/java/christmas/domain/Badge.java
+++ src/main/java/christmas/domain/Badge.java
@@ -1,12 +1,23 @@
 package christmas.domain;
 
 public enum Badge {
-    STAR(5000, "별"), TREE(10000, "트리"), SANTA(20000, "산타");
+    STAR(5000, "별"), TREE(10000, "트리"), SANTA(20000, "산타"), NONE(0, "없음");
     private final int price;
     private final String badgeName;
 
     Badge(int price, String badgeName) {
         this.price = price;
         this.badgeName = badgeName;
+    }
+
+    public static Badge getBadge(int benefitAmount) {
+        if (benefitAmount >= SANTA.price) return SANTA;
+        if (benefitAmount >= TREE.price) return TREE;
+        if (benefitAmount >= STAR.price) return STAR;
+        return NONE;
+    }
+
+    public String getBadgeName() {
+        return this.badgeName;
     }
 }
