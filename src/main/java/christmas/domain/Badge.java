cdd30f487bd4e0dd67ff87a42a03b07a8b4eb925
bokyung <bbookng@gmail.com>
2023-11-13 12:25:13 +0900

    feat(Badge) : add Badge Class

--- src/main/java/christmas/domain/Badge.java
+++ src/main/java/christmas/domain/Badge.java
@@ -1,4 +1,12 @@
 package christmas.domain;
 
 public enum Badge {
+    STAR(5000, "별"), TREE(10000, "트리"), SANTA(20000, "산타");
+    private final int price;
+    private final String badgeName;
+
+    Badge(int price, String badgeName) {
+        this.price = price;
+        this.badgeName = badgeName;
+    }
 }
