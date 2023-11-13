4f8549437c9d20f19dc63476e51d5aa683dbf4e3
bokyung <bbookng@gmail.com>
2023-11-13 12:26:05 +0900

    feat(InputView) : create inputOrderList Method

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -3,8 +3,15 @@ package christmas.view;
 import camp.nextstep.edu.missionutils.Console;
 import christmas.common.ErrorMessages;
 import christmas.common.Messages;
+import christmas.domain.OrderList;
+import christmas.domain.OrderListFactory;
 
 public class InputView {
+    private final OrderListFactory orderListFactory;
+
+    public InputView() {
+        this.orderListFactory = new OrderListFactory();
+    }
 
     public int inputVisitDay() {
         System.out.println(Messages.inputVisitDay);
@@ -13,8 +20,14 @@ public class InputView {
         return Integer.parseInt(input);
     }
 
+    public OrderList inputOrderList() {
+        System.out.println(Messages.inputOrderMenu);
+        String input = Console.readLine();
+        return orderListFactory.createOrderList(input);
+    }
+
     private void validateIsNumber(String input) {
         if (!input.matches("[0-9]"))
-            throw new IllegalArgumentException(ErrorMessages.validateIsNumber);
+            throw new IllegalArgumentException(ErrorMessages.validateVisitDay);
     }
 }
