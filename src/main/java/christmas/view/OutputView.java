2082d65f397776877903c0de8947c2673e17642c
bokyung <bbookng@gmail.com>
2023-11-13 17:36:01 +0900

    feat(OutputView) : eventPreview 출력 추가

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -3,7 +3,10 @@ package christmas.view;
 import christmas.common.Messages;
 
 public class OutputView {
-    public void printIntroMessage() {
+    public void printGreetingMessage() {
         System.out.println(Messages.greetingMessage);
+    }
+    public void printEventPreview(int visitDay) {
+        System.out.println(String.format(Messages.eventPreview, visitDay));
     }
 }
