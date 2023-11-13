1969ab0ddb430dca7b4ea80b64d3f04980087c9f
bokyung <bbookng@gmail.com>
2023-11-13 17:36:56 +0900

    feat : 사용자 input 값까지 controller 로직 구현

--- src/main/java/christmas/controller/ChristmasController.java
+++ src/main/java/christmas/controller/ChristmasController.java
@@ -1,4 +1,17 @@
 package christmas.controller;
 
+import christmas.domain.OrderList;
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
 public class ChristmasController {
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+    public void play() {
+        outputView.printGreetingMessage();
+        int visitDay = inputView.inputVisitDay();
+        OrderList orderList = inputView.inputOrderList();
+        outputView.printEventPreview(visitDay);
+
+    }
 }
