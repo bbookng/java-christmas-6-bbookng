cff0df56042e0e8a7701eccccdb0877edf99a754
bokyung <bbookng@gmail.com>
2023-11-14 10:34:50 +0900

    feat(OutputView) : 주문 목록 및 결과값 출력 메서드 추가

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,6 +1,8 @@
 package christmas.view;
 
 import christmas.common.Messages;
+import christmas.domain.OrderList;
+import christmas.domain.TotalOrder;
 
 public class OutputView {
     public void printGreetingMessage() {
@@ -8,5 +10,11 @@ public class OutputView {
     }
     public void printEventPreview(int visitDay) {
         System.out.println(String.format(Messages.eventPreview, visitDay));
+    }
+    public void printOrderList(OrderList orderList) {
+        System.out.println(orderList.toString());
+    }
+    public void printResultMessage(TotalOrder totalOrder) {
+        System.out.println(totalOrder.toString());
     }
 }
