8950983598a2728c07aafa25d95525f1dc94b3b2
bokyung <bbookng@gmail.com>
2023-11-14 11:45:15 +0900

    feat(ErrorMessages) : 전체 주문 유효 조건 에러 메시지 추가

--- src/main/java/christmas/common/ErrorMessages.java
+++ src/main/java/christmas/common/ErrorMessages.java
@@ -3,4 +3,6 @@ package christmas.common;
 public class ErrorMessages {
     public static String validateVisitDay = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
     public static String validateOrderMenu = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
+    public static String validateOrderWithDrink = "[ERROR] 음료만 주문시, 주문할 수 없습니다. 다시 입력해 주세요.";
+    public static String validateOrderMenuCounts = "[ERROR] 메뉴는 한 번에 최대 20개까지만 주문할 수 있습니다. 다시 입력해 주세요.";
 }
