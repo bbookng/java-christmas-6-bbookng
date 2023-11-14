46e996a4792c2115c357d96d3a8ad2bc659e0521
bokyung <bbookng@gmail.com>
2023-11-14 11:45:35 +0900

    feat(Constant) : 음료만 주문했을 시 예외를 반환하기 위한 상수 추가

--- src/main/java/christmas/common/Constant.java
+++ src/main/java/christmas/common/Constant.java
@@ -1,5 +1,8 @@
 package christmas.common;
 
+import christmas.domain.MenuType;
+
 public class Constant {
     public static int[] STARRED_DAY = new int[] {3, 10, 17, 24, 25, 31};
+    public static MenuType[] MENU_TYPES = new MenuType[] {MenuType.APPETIZER, MenuType.MAIN, MenuType.DESERT};
 }
